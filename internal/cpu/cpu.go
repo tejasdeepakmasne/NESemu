2b721024e726a8d7b0c17a3df7c5482a976af255
Shivansh Bhatnagar <shivansh.bhatnagar.mat22@itbhu.ac.in>
2024-06-02 14:50:52 +0530

    Added some basic structures, registers and Addressing Modes

--- internal/cpu/cpu.go
+++ internal/cpu/cpu.go
@@ -1,16 +1,68 @@
 package cpu
 
 // CPU represents the Central Processing Unit of the NES.
+
+const NonMaskableInterruptVector uint16 = 0xFFFA
+const ResetVector uint16 = 0xFFFC
+const InterruptRequestVector uint16 = 0xFFFE
+const StackBase uint16 = 0x0100
+const StackReset uint8 = 0xFD
+
 type CPU struct {
-	// Define CPU state and registers here
-	// For example:
-	// A uint8
-	// X uint8
-	// Y uint8
-	// PC uint16
-	// SP uint8
-	// P uint8 (Processor Status Register)
+	accumulator uint8
+	xIndex uint8
+	yIndex uint8
+	stackPointer uint8
+	programCounter uint16
+	statusRegister uint8
+
+	memory []uint8
 }
+
+type Flags uint8
+
+const (
+	C Flags = iota// Carry
+	Z // Zero
+	I // Interrupt Disable
+	D // Decimal Mode
+	B // B Flag
+	X // Unused
+	V // Overflow
+	N // Negative
+)
+
+type AddressingMode int
+
+const (
+	modeImmediate AddressingMode = iota
+	modeZeroPage
+	modeAbsolute
+	modeZeroPageX
+	modeZeroPageY
+	modeAbsoluteX
+	modeAbsoluteY
+	modeIndirectX
+	modeIndirectY
+	modeRelative
+	modeAccumulator
+	modeIndirect
+	modeNoneAddressing
+)
+
+func (c *CPU) readMemory(address uint16) uint8 {
+	// Implement memory read logic here
+}
+func (c *CPU) writeMemory(address uint16, value uint8) {
+	// Implement memory write logic here
+}
+func (c *CPU) pushStack(value uint8) {
+	// Implement stack push logic here
+}
+func (c *CPU) popStack() uint8 {
+	// Implement stack pop logic here
+}
+
 
 // NewCPU creates and initializes a new CPU instance.
 func NewCPU() *CPU {
