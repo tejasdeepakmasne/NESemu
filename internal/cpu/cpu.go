95be3462ee30d4cf46be8c6102c0cbc7b664d395
Shivansh Bhatnagar <shivansh.bhatnagar.mat22@itbhu.ac.in>
2024-06-13 01:39:43 +0530

    Added the addressing mode implementation

--- internal/cpu/cpu.go
+++ internal/cpu/cpu.go
@@ -99,48 +99,71 @@ func (c *CPU) pushStack16(value uint16) {
 	c.memory[StackBase+uint16(c.stackPointer)] = msb
 	c.stackPointer--
 }
-
-func (c *CPU) addressMode(mode AddressingMode) (uint16, uint16) {
+func (c *CPU) addressMode(mode AddressingMode) uint16 {
+	var address uint16
 	switch mode {
 	case modeImmediate:
 		// Immediate addressing mode: The operand is the next byte after the instruction.
 		// Implement logic to fetch the operand from memory and return the address.
+		address = c.programCounter
 
 	case modeZeroPage:
 		// Zero Page addressing mode: The operand is the byte at the zero page address.
 		// Implement logic to fetch the operand from memory and return the address.
+		address = uint16(c.readMemory(c.programCounter))
 
 	case modeAbsolute:
 		// Absolute addressing mode: The operand is the byte at the specified address.
 		// Implement logic to fetch the operand from memory and return the address.
+		address = c.readMemory16(c.programCounter)
 
 	case modeZeroPageX:
 		// Zero Page X addressing mode: The operand is the byte at the zero page address plus the value of the X register.
 		// Implement logic to fetch the operand from memory and return the address.
+		base_address := c.readMemory(c.programCounter)
+		address = uint16(base_address + c.xIndex)
 
 	case modeZeroPageY:
 		// Zero Page Y addressing mode: The operand is the byte at the zero page address plus the value of the Y register.
 		// Implement logic to fetch the operand from memory and return the address.
+		base_address := c.readMemory(c.programCounter)
+		address = uint16(base_address + c.yIndex)
 
 	case modeAbsoluteX:
 		// Absolute X addressing mode: The operand is the byte at the specified address plus the value of the X register.
 		// Implement logic to fetch the operand from memory and return the address.
+		base_address := c.readMemory16(c.programCounter)
+		address = base_address + uint16(c.xIndex)
 
 	case modeAbsoluteY:
 		// Absolute Y addressing mode: The operand is the byte at the specified address plus the value of the Y register.
 		// Implement logic to fetch the operand from memory and return the address.
+		base_address := c.readMemory16(c.programCounter)
+		address = base_address + uint16(c.yIndex)
 
 	case modeIndirectX:
 		// Indirect X addressing mode: The operand is the byte at the address formed by adding the X register to the zero page address.
 		// Implement logic to fetch the operand from memory and return the address.
+		base := c.readMemory(c.programCounter)
+		var offset uint8 = base + c.xIndex
+		lsb := c.readMemory(uint16(offset))
+		msb := c.readMemory(uint16(offset + 1))
+		address = uint16(msb)<<8 | uint16(lsb)
+
 
 	case modeIndirectY:
 		// Indirect Y addressing mode: The operand is the byte at the address formed by adding the Y register to the zero page address.
 		// Implement logic to fetch the operand from memory and return the address.
+		base := c.readMemory(c.programCounter)
+		var offset uint8 = base + c.yIndex
+		lsb := c.readMemory(uint16(offset))
+		msb := c.readMemory(uint16(offset + 1))
+		address = uint16(msb)<<8 | uint16(lsb)
 
 	case modeRelative:
 		// Relative addressing mode: The operand is a signed 8-bit offset relative to the program counter.
 		// Implement logic to calculate the target address based on the offset and return it.
+		address = c.programCounter
 
 	case modeAccumulator:
 		// Accumulator addressing mode: The operand is the accumulator register itself.
@@ -149,13 +172,22 @@ func (c *CPU) addressMode(mode AddressingMode) (uint16, uint16) {
 	case modeIndirect:
 		// Indirect addressing mode: The operand is the address stored at the specified address.
 		// Implement logic to fetch the operand from memory and return the address.
+		lsb := uint16(c.readMemory(c.programCounter))
+		msb := uint16(c.readMemory(c.programCounter + 1))
+		indirectVector := (msb << 8) | lsb
+		address_lsb := uint16(c.readMemory(indirectVector))
+		address_msb := uint16(c.readMemory(indirectVector + 1))
+		if indirectVector & 0x00ff == 0x00ff {
+			address_msb = address_lsb & 0xff00
+		}
+		address = (address_msb << 8) | address_lsb
 
 	case modeNoneAddressing:
 		// No addressing mode: The instruction does not have an operand.
 		// No additional logic is needed, simply return 0 for both addresses.
 	}
 
-	return 0, 0
+	return address
 }
 
 // NewCPU creates and initializes a new CPU instance.
