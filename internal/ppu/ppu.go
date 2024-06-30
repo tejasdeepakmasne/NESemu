ce454e02e0561320c6770798d35095830cd0a9aa
4pahuja <98739520+4pahuja@users.noreply.github.com>
2024-06-30 13:59:57 +0530

    Defined PPU registers and state

--- internal/ppu/ppu.go
+++ internal/ppu/ppu.go
@@ -6,7 +6,38 @@ type PPU struct {
 	// For example:
 	// VRAM []uint8
 	// OAM []uint8
-	// Registers struct { ... }
+	Registers struct {
+		ppuCtrl uint8
+		ppuMask uint8
+		ppuStatus uint8
+		oamAddr uint8
+		oamData uint8
+		ppuScroll uint8
+		ppuAddr uint8
+		ppuData uint8
+	}
+	internReg struct {
+		v uint16
+		t uint16
+		x uint8
+		w bool
+	}
+	type tile struct{
+		plane1 uint64
+		plane2 uint64
+	}
+	//Left and right pattern tables(CHR-ROM/RAM)
+	lptrnTable [256]tile
+	rptrnTable [256]tile
+	//4 Name tables
+	nTable1 [32][32]uint8
+	nTable2 [32][32]uint8
+	nTable3 [32][32]uint8
+	nTable4 [32][32]uint8
+	
+	OAM [64]uint32
+	VRAM []uint8
+	pallete [32]uint8
 }
 
 // NewPPU creates and initializes a new PPU instance.
